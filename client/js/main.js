72e96ed47279a2d923b5c36fd8527e984a9ff486
krbz'11 <karby92@gmail.com>
2015-05-28 21:46:04 +0200

    Pins are working right.

---
+++ client/js/main.js
@@ -0,0 +1,84 @@
+var modules = {
+    variables: function(){
+        var draw = document.getElementById('draw');
+    },
+    init: function() {
+        modules.variables;
+        draw.addEventListener('click', modules.pin.init);
+    },
+    pin: {
+        init: function(ev) {
+            if (ev.target.classList.contains('circ')) {
+                ev.target.addEventListener('click', modules.pin.commentArea(ev));
+            } else if (ev.target === draw) {
+                modules.pin.closeAllCommentArea();
+                modules.pin.addNew(ev);
+            }
+        },
+        addNew: function(ev) {
+                var drawY = draw.offsetTop, //20
+                    drawX = draw.offsetLeft; //130~
+                var span = document.createElement("span");
+                    span.classList.add('circ');
+                    span.style.left = (ev.pageX-drawX-18)+'px';
+                    span.style.top = (ev.pageY-drawY-18)+'px';
+                    span.innerHTML = '<div class="commentArea"><p class="comment-list">Some lorem</p></div>';
+                draw.appendChild(span)
+                //innerHTML z json'a export wateva
+        },
+        commentArea: function(ev) {
+            if (ev.target.querySelector('.commentArea').classList.contains('displayNone')) {
+                modules.pin.closeAllCommentArea();
+                ev.target.querySelector('.commentArea').classList.remove('displayNone');
+                ev.target.classList.add('activePin');
+            } else {
+                ev.target.querySelector('.commentArea').classList.add('displayNone');
+            }
+        },
+        closeAllCommentArea: function() {
+            var circs = document.querySelectorAll('.circ');
+            for (var i=0; i < circs.length; i++) {
+                circs[i].querySelector('.commentArea').classList.add('displayNone');
+                circs[i].classList.remove('activePin');
+            }
+        }
+   } 
+};
+                    //wyswietla komentarz
+                    //mozliwosc edycji
+var io = io();
+modules.init();
+
+
+//TODO
+
+//pinz
+//dodaje pin do obj, pobiera dane z json'a jako templatka,
+//kazdy pin losuje _id
+//kazdy _id ma swoje komentarze + score system (feature)
+//Score system - dodaje usera do komentarza + blokuje mozliwosc ponownego oceniania 
+//for each != user.name = score available + emit
+//...
+//Toggle na pinie + close X inside.
+//Templatka do commentArea + inputy do komentarza + komentowania
+//
+
+
+//login
+//login system, moze jakies auth via gmail, fb, tw, github
+//...
+
+//
+
+
+
+
+
+// var colors = ['#b00b00', '#de1e7e', '#e1e100', '#BADA55', '#F0FEAF', '#ac1d1c', '#facade'];
+
+
+//They see me rolling, they hating.. 
+//Stop propagation bad behaviours..
+//Stop making excuses..
+
+
---
+++ server.js
@@ -0,0 +1,71 @@
+var http = require('http');
+var path = require('path');
+var socketio = require('socket.io');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+var io = socketio.listen(server);
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var url = 'mongodb://karbz:shinev234@ds031842.mongolab.com:31842/nodedb';
+
+var points = [
+        {
+            left: 230,
+            top: 201
+        },
+        {
+            left: 50,
+            top: 102
+        }
+    ];
+
+
+app.use(express.static(path.resolve(__dirname, 'client')));
+
+io.on('connection', function (socket) {
+    console.log('User is connected', socket.id);
+    
+
+    socket.on('disconnect', function () {
+      console.log("User left", socket.id);
+    });
+
+    MongoClient.connect(url, function (error, db) {
+        if (error) {
+          console.log('#error - Unable to connect to the mongoDB server. Error:', error);
+        } else {
+          console.log('#info - Connection established to', url);
+        }
+        //broadcast data from db - 
+        //emit it.
+        //if new click, main.js - emit it to db.
+        //
+        var pointzCollection = db.collection('pointz');
+        // userCollection.insert({"details":data}, function(error, cursor){
+            // cursor ? console.log('#success - data send to db') : console.log('#error -');
+        // });
+    });
+
+
+
+});
+
+
+
+
+
+
+
+// function broadcast(event, data) {
+//   sockets.forEach(function (socket) {
+//     socket.emit(event, data);
+//   });
+// }
+
+
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Chat server listening at", addr.address + ":" + addr.port);
+});
